0a9c94c409d55c7c2b889f48410ae303e4d6a3e5
neuanwi <windhkhj1@gmail.com>
2024-06-09 00:30:43 +0900

    plz

--- test1.cpp
+++ test1.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include<iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-	cout << "haaaaa";
+
+int main(void) {
+
+	int a[3];
+	for (int i = 0; i < 3; i++) cin >> a[i];
+	int hap[10];
+	fill(hap, hap + 10, 0);
+
+	string sum = to_string(a[0] * a[1] * a[2]);
+	for (int i = 0; i < sum.length(); i++) hap[sum[i]-'0']++;
+	
+	for (int i = 0; i < 10; i++) cout << hap[i] << '\n';
+	
+	return 0;
 }
