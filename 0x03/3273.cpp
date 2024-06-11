912852efae01707c756139699e83a2c29df7b338
neuanwi <windhkhj1@gmail.com>
2024-06-11 19:26:45 +0900

    4

---
+++ 0x03/3273.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+int a[2000001]={};
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, t, x, ans=0;
+  cin >> n;
+  for (int i=0; i<n; ++i) {
+    cin >> t;
+    a[t]++;
+  }
+
+  cin >> x;
+  for (int i = 1; i < (x+1)/2; ++i) {
+    if (a[i]==1 && a[x-i]==1) ans++;
+  }
+  cout << ans;
+}
