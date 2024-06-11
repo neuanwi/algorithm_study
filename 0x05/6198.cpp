800ce9ccb62c92db48d610801fba40f9f7b23daa
neuanwi <windhkhj1@gmail.com>
2024-06-11 19:36:26 +0900

    5

---
+++ 0x05/6198.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+
+stack<int> s;
+int n;
+ll ans;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    ll h;
+    while (n--) {
+        cin >> h;
+        while (!s.empty() && s.top() <= h)
+            s.pop();
+        ans += s.size();
+        s.push(h);
+    }
+    cout << ans;
+    return 0;
+}
